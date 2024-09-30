ab92243c73270763751d47ab7a647d96dfa0d03a
Kaushik Manivannan <manivannan.k@northeastern.edu>
2024-09-29 21:17:48 -0400

    Chore: Declare User Controller Functions

---
+++ src/controllers/userController.js
@@ -0,0 +1,11 @@
+export const createUser = async (req, res) => {
+  return res.status(201).send();
+}
+
+export const getUser = async (req, res) => {
+  return res.status(200).send();
+}
+
+export const updateUser = async (req, res) => {
+  return res.status(204).send();
+}
