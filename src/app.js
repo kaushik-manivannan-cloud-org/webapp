ba5e204f41b1f5dd1f8579771fbf8680cf91eedd
Kaushik Manivannan <manivannan.k@northeastern.edu>
2024-10-02 03:31:27 -0400

    Fix: Remove Redundant Logs

--- src/app.js
+++ src/app.js
@@ -5,19 +5,10 @@ import logger from './utils/logger.js';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`);
-});
-
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
 registerRoutes(app);
 
-process.on("uncaughtException", (err) => {
-  logger.error("Uncaught Exception", { error: err });
+app.listen(PORT, () => {
+  logger.info(`Server running on port ${PORT}`);
 });
-
-process.on("unhandledRejection", (err) => {
-  logger.error("Unhandled Rejection", { error: err });
-});
-
