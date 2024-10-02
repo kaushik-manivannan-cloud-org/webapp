df94b46ec5f8bd267c1d41a98e3e5a3e6b5cb4e1
Kaushik Manivannan <manivannan.k@northeastern.edu>
2024-10-02 18:34:33 -0400

    Feat: Add App Wide No Cache and 404 Handler

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
 import express from 'express';
 import registerRoutes from './routes/index.js';
 import logger from './utils/logger.js';
+import { pageNotFound } from './middleware/pageNotFound.js';
+import noCache from './middleware/noCache.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
+app.use(noCache);
 registerRoutes(app);
+app.use(pageNotFound);
 
 app.listen(PORT, () => {
   logger.info(`Server running on port ${PORT}`);
