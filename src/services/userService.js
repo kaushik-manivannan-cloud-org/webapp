d611b3f4751f8bcb754bdf90339d8e2fc21b335a
Kaushik Manivannan <manivannan.k@northeastern.edu>
2024-10-02 02:05:45 -0400

    Feat: Add Get User Service

--- src/services/userService.js
+++ src/services/userService.js
@@ -10,3 +10,16 @@ export const createUser = async (userData) => {
     throw error;
   }
 };
+
+export const getUser = async (email) => {
+  try {
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      logger.warn('User not found', { email });
+      throw new Error('User not found')
+    }
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
