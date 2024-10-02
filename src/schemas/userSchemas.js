5834615d8613086155894a80035c4e3f01e652fe
Kaushik Manivannan <manivannan.k@northeastern.edu>
2024-10-02 18:57:41 -0400

    Feat: Update User Schemas for Optional Fields

--- src/schemas/userSchemas.js
+++ src/schemas/userSchemas.js
@@ -5,10 +5,23 @@ export const createUserSchema = Joi.object({
   last_name: Joi.string().required(),
   email: Joi.string().email().required(),
   password: Joi.string().required(),
+  account_created: Joi.any().optional(),
+  account_updated: Joi.any().optional(),
 })
 
 export const updateUserSchema = Joi.object({
-  first_name: Joi.string(),
-  last_name: Joi.string(),
-  password: Joi.string()
-}).min(1);
+  first_name: Joi.string().allow(''),
+  last_name: Joi.string().allow(''),
+  password: Joi.string().allow(''),
+  account_created: Joi.any().optional(),
+  account_updated: Joi.any().optional(),
+})
+.custom((obj, helpers) => {
+  if (!obj.first_name && !obj.last_name && !obj.password) {
+    return helpers.error('object.atLeastOneRequired');
+  }
+  return obj;
+}, 'At least one field check')
+.messages({
+  'object.atLeastOneRequired': 'At least one of first_name, last_name, or password must be provided'
+});;
