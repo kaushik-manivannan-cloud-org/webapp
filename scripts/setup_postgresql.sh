26b3300e94723ca77d10fad07886407600ffd9e4
Kaushik Manivannan <manivannan.k@northeastern.edu>
2024-10-22 17:01:40 -0400

    Chore: Remove Local DB Installation

--- scripts/setup_postgresql.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-# Use environment variables directly
-sudo -u postgres psql -c "CREATE USER ${DB_USERNAME} WITH SUPERUSER CREATEDB PASSWORD '${DB_USERNAME}';"
-sudo -u postgres psql -c "CREATE DATABASE ${DB_NAME} OWNER ${DB_USERNAME};"
