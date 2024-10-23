91c0b587a730c5092f7d946fd116c8beec0ea3ee
Kaushik Manivannan <manivannan.k@northeastern.edu>
2024-10-22 20:18:45 -0400

    Fix: Create Empty Env File

--- scripts/set_env_file.sh
+++ scripts/set_env_file.sh
@@ -2,14 +2,7 @@
 set -e
 
 # Create the environment file
-sudo bash -c 'cat > /opt/webapp/.env' << 'EOL'
-# Database Configuration
-DB_HOST=
-DB_PORT=
-DB_NAME=
-DB_USER=
-DB_PASSWORD=
-EOL
+sudo touch /opt/webapp/.env
 
 # Set proper permissions for the env file
 sudo chown csye6225:csye6225 /opt/webapp/.env
